a455d5278cc14add4e3a0e82d0c89c41799c6bf7
furkannnyzg <maxuxzone@gmail.com>
2024-09-01 23:52:10 +0300

    Constructorlar ve gerekli değişkenler tanımlandı.

---
+++ sinemaUygulama/Models/Session.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinemaUygulama.Models
+{
+    public class Session
+    {
+
+        /*date,time,chair*/
+        public  Session() {
+        SetDefaultChairs();
+        }
+        public string date { get; set; }
+        public string time { get; set; }
+        public List<chair> chairs { get; set; }
+
+        private void SetDefaultChairs(){
+        
+            chairs = new List<chair>();
+            string[] rows = {"a","b","c","d"};
+            string[] numbers = {"1","2","3","4","5","6"};
+
+            foreach (string row in rows) {
+
+                foreach (string number in numbers) {
+               
+                    chair chair = new chair(row,number);
+                    chairs.Add(chair);
+                }
+            }
+        
+        }
+    }
+}
---
+++ sinemaUygulama/Models/chair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinemaUygulama.Models
+{
+    public class chair
+    {
+         public chair() { }
+        public chair(string row,string number) {
+            this.Row = row;
+            this.Number = number;
+            
+        }
+
+        public string Row { get; set; }
+        public string Number { get; set; }
+        public bool Status { get; set; }
+    }
+}
